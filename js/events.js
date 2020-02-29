afa78d5b7ec7d3aaec45011bba10813c301de669
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2020-02-29 18:24:30 +0530

    Sub events section modified

--- js/events.js
+++ js/events.js
@@ -1,116 +1,34 @@
-/**
-* Template Name: Mamba - v2.0.1
-* Template URL: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-!(function($) {
-    "use strict";
-  
-    // Toggle .header-scrolled class to #header when page is scrolled
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('#header').addClass('header-scrolled');
-      } else {
-        $('#header').removeClass('header-scrolled');
-      }
-    });
-  
-    if ($(window).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-    }
-  
-    // Stick the header at top on scroll
-    $("#header").sticky({
-      topSpacing: 0,
-      zIndex: '50'
-    });
-  
-    // Smooth scroll for the navigation menu and links with .scrollto classes
-    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        e.preventDefault();
-        var target = $(this.hash);
-        if (target.length) {
-  
-          var scrollto = target.offset().top;
-          var scrolled = 2;
-  
-          if ($('#header-sticky-wrapper').length) {
-            scrollto -= $('#header-sticky-wrapper').outerHeight() - scrolled;
-          }
-  
-          if ($(this).attr("href") == '#header') {
-            scrollto = 0;
-          }
-  
-          $('html, body').animate({
-            scrollTop: scrollto
-          }, 1500, 'easeInOutExpo');
-  
-          if ($(this).parents('.nav-menu, .mobile-nav').length) {
-            $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-            $(this).closest('li').addClass('active');
-          }
-  
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-            $('.mobile-nav-overly').fadeOut();
-          }
-          return false;
-        }
-      }
-    });
-  
-    // Mobile Navigation
-    if ($('.nav-menu').length) {
-      var $mobile_nav = $('.nav-menu').clone().prop({
-        class: 'mobile-nav d-lg-none'
-      });
-      $('body').append($mobile_nav);
-      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-      $('body').append('<div class="mobile-nav-overly"></div>');
-  
-      $(document).on('click', '.mobile-nav-toggle', function(e) {
-        $('body').toggleClass('mobile-nav-active');
-        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-        $('.mobile-nav-overly').toggle();
-      });
-  
-      $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-        e.preventDefault();
-        $(this).next().slideToggle(300);
-        $(this).parent().toggleClass('active');
-      });
-  
-      $(document).click(function(e) {
-        var container = $(".mobile-nav, .mobile-nav-toggle");
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-            $('.mobile-nav-overly').fadeOut();
-          }
-        }
-      });
-    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-      $(".mobile-nav, .mobile-nav-toggle").hide();
-    }
-  
-    // Intro carousel
-    var heroCarousel = $("#heroCarousel");
-    var heroCarouselIndicators = $("#hero-carousel-indicators");
-    heroCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
-      (index === 0) ?
-      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>"):
-        heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
-    });
-  
-    heroCarousel.on('slid.bs.carousel', function(e) {
-      $(this).find('h2').addClass('animated fadeInDown');
-      $(this).find('p').addClass('animated fadeInUp');
-      $(this).find('.btn-get-started').addClass('animated fadeInUp');
-    });
-  
-  })(jQuery);
+let event1 = document.getElementById("event1")
+event1.addEventListener("click", ()=>{
+        localStorage.setItem("subEvent", event1.classList[0])
+})
+
+let event2 = document.getElementById("event2")
+event2.addEventListener("click", ()=>{
+        localStorage.setItem("subEvent", event2.classList[0])
+})
+
+let event3 = document.getElementById("event3")
+event3.addEventListener("click", ()=>{
+        localStorage.setItem("subEvent", event3.classList[0])
+})
+
+let event4 = document.getElementById("event4")
+event4.addEventListener("click", ()=>{
+        localStorage.setItem("subEvent", event4.classList[0])
+})
+
+let event5 = document.getElementById("event5")
+event5.addEventListener("click", ()=>{
+        localStorage.setItem("subEvent", event5.classList[0])
+})
+
+let event6 = document.getElementById("event6")
+event6.addEventListener("click", ()=>{
+        localStorage.setItem("subEvent", event6.classList[0])
+})
+
+let event7 = document.getElementById("event7")
+event7.addEventListener("click", ()=>{
+        localStorage.setItem("subEvent", event7.classList[0])
+})
