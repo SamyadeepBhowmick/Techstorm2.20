812e8ca4c9d0a270bb90b22d75fb2e3a9ef6bd20
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2020-03-01 10:22:23 +0530

    Event Modal integrated

--- js/events.js
+++ js/events.js
@@ -8,6 +8,24 @@
 
 function register(name){
         localStorage.setItem("subEvent", name)
-        window.open("register.html",'_blank')
+        openModal()
+        // window.open("register.html",'_blank')
         //modal will be called from here.
+}
+modal = document.getElementById("modal")
+// btn.addEventListener("click", openModal)
+modal.addEventListener("click", closeModal)
+
+function openModal(){
+    if(modal.classList[1] === "out"){
+        modal.classList.remove("out")
+    }
+    modal.classList.add("active")
+}
+
+function closeModal(e){
+    if (e.target == modal) {
+        modal.classList.remove("active")
+        modal.classList.add("out")
+    }
 }
